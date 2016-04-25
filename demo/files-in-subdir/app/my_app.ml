0b9299ee8fd468d025bfd0190b63daa9ac205128
Ashish Agarwal <agarwal1975@gmail.com>
2016-04-25 16:19:24 -0400

    add another demo

---
+++ demo/files-in-subdir/app/my_app.ml
@@ -0,0 +1,4 @@
+open Printf
+open My_project
+
+let () = printf "B.y = %d\n" B.y
---
+++ demo/files-in-subdir/myocamlbuild.ml
@@ -0,0 +1,18 @@
+open Printf
+open Solvuu_build.Std
+
+let project_name = "my_project"
+let version = "dev"
+
+let lib = Item.lib project_name
+  ~dir:"lib"
+  ~pack_name:project_name
+  ~pkg:project_name
+
+let app = Item.app "my_app"
+  ~file:"app/my_app.ml"
+
+let project = Project.make ~name:project_name ~version [lib;app]
+
+;;
+let () = Project.dispatch project
