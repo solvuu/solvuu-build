60a111566edce0a87fbd0da67c75ce010ff1e004
Ashish Agarwal <agarwal1975@gmail.com>
2016-05-11 13:11:45 -0400

    demo/multi: add thread option
    Now required because it is no longer hard coded as of
    904b1b9822d2f3e59862db37bf5205f0aaf05a02.

--- demo/multi/myocamlbuild.ml
+++ demo/multi/myocamlbuild.ml
@@ -4,12 +4,17 @@ open Solvuu_build.Std
 let name = "my_project"
 let version = "dev"
 
+let thread = ()
+
 let make_lib ?findlib_deps ?internal_deps ?build_if lib_name =
   Item.lib (sprintf "%s_%s" name lib_name)
+    ~thread
     ?findlib_deps ?internal_deps ?build_if
     ~dir:(sprintf "lib/%s" lib_name)
     ~pack_name:(sprintf "%s_%s" name lib_name)
     ~pkg:(sprintf "%s.%s" name lib_name)
+
+let make_app = Item.app ~thread
 
 let async = make_lib "async"
   ~findlib_deps:["async"]
@@ -17,7 +22,7 @@ let async = make_lib "async"
 let lwt = make_lib "lwt"
   ~findlib_deps:["lwt"] ~build_if:[`Pkgs_installed]
 
-let app = Item.app "my_app"
+let app = make_app "my_app"
   ~file:"app/my_app.ml"
   ~internal_deps:[async]
 
