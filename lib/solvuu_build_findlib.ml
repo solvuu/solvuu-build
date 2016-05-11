34bd7bbdb228460de434e206282bb91dd30ca737
Ashish Agarwal <agarwal1975@gmail.com>
2016-05-11 17:08:26 -0400

    Findlib: add graph operations over findlib package names

--- lib/solvuu_build_findlib.ml
+++ lib/solvuu_build_findlib.ml
@@ -1,7 +1,5 @@
 open Printf
-module Util = Util
-module List = Util.List
-module String = Util.String
+open Util
 
 type pkg = string
 
@@ -19,3 +17,82 @@ let to_use_tag x =
 
 let to_path x =
   String.split x ~on:'.'
+
+
+(******************************************************************************)
+(** {2 Graph Operations} *)
+(******************************************************************************)
+module T = struct
+  type nonrec t = pkg
+  let compare = compare
+  let hash = Hashtbl.hash
+  let equal = (=)
+end
+
+module Graph = struct
+  include Graph.Persistent.Digraph.Concrete(T)
+
+  module Dfs = Graph.Traverse.Dfs(
+    Graph.Persistent.Digraph.Concrete(T)
+  )
+
+  module Topological = struct
+    include Graph.Topological.Make(
+        Graph.Persistent.Digraph.Concrete(T)
+      )
+
+    let sort g = fold (fun x l -> x::l) g []
+  end
+
+  module Gml = struct
+    include Graph.Gml.Print
+        (Graph.Persistent.Digraph.Concrete(T))
+        (struct
+          let node x  = [x, Graph.Gml.List []]
+          let edge () = ["", Graph.Gml.List []]
+        end)
+
+    let print t =
+      let buf = Buffer.create 30 in
+      let fmt = Format.formatter_of_buffer buf in
+      print fmt t;
+      Buffer.contents buf
+
+  end
+
+  let roots g =
+    fold_vertex
+      (fun x accum -> if in_degree g x = 0 then x::accum else accum)
+      g
+      []
+
+  let of_list xs =
+    let concat (prefix:string list) (x:string) =
+      String.concat "." (prefix@[x])
+    in
+    if not (List.is_uniq ~cmp:T.compare xs) then
+      failwith "multiple findlib packages have an identical name"
+    else
+      let g =
+        List.fold_left xs ~init:empty ~f:(fun g x ->
+          let rec loop g prefix parts = match parts with
+            | [] -> g
+            | x::[] -> (
+                let x' = concat prefix x in
+                add_vertex g x'
+              )
+            | x::y::l -> (
+                let x' = concat prefix x in
+                let y' = concat (prefix@[x]) y in
+                loop (add_edge g x' y') (prefix@[x]) (y::l)
+              )
+          in
+          loop g [] (to_path x)
+        )
+      in
+      if Dfs.has_cycle g then
+        failwith "findlib package names form a cycle"
+      else
+        g
+
+end
--- lib/solvuu_build_findlib.mli
+++ lib/solvuu_build_findlib.mli
@@ -13,3 +13,43 @@ val to_use_tag : pkg -> string
 
 (** Split [pkg] on dots to expose its path structure. *)
 val to_path : pkg -> string list
+
+
+(******************************************************************************)
+(** {2 Graph Operations} *)
+(******************************************************************************)
+
+(** Items types and functions wrapped into a module. *)
+module T : sig
+  type t = pkg
+  val compare : t -> t -> int
+  val equal : t -> t -> bool
+  val hash : t -> int
+end
+
+module Graph : sig
+  include module type of Graph.Persistent.Digraph.Concrete(T)
+
+  module Dfs : module type of Graph.Traverse.Dfs(
+    Graph.Persistent.Digraph.Concrete(T)
+  )
+
+  module Topological : sig
+    include module type of Graph.Topological.Make(
+      Graph.Persistent.Digraph.Concrete(T)
+    )
+
+    (** Return a topologically sorted vertex list of given graph. *)
+    val sort : t -> V.t list
+  end
+
+  module Gml : sig
+    val print : t -> string
+  end
+
+  val roots : t -> V.t list
+
+  (** Construct a graph from a list of items. Raise exception if there
+      are cycles or any other errors. *)
+  val of_list : V.t list -> t
+end
