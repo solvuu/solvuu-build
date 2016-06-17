d95fb8d76f0aaafccc090e38d3a99197a82a48d0
Ashish Agarwal <agarwal1975@gmail.com>
2016-06-17 10:23:44 -0400

    demo/multi: make it work again
    Updated as needed due to changes in main lib.

--- demo/multi/myocamlbuild.ml
+++ demo/multi/myocamlbuild.ml
@@ -1,32 +1,37 @@
 open Printf
 open Solvuu_build.Std
+open Solvuu_build.Util
 
-let name = "my_project"
+let project_name = "my_project"
 let version = "dev"
 
 let thread = ()
 
-let make_lib ?findlib_deps ?internal_deps ?build_if lib_name =
-  Item.lib (sprintf "%s_%s" name lib_name)
+let make_lib ?findlib_deps ?internal_deps lib_name =
+  Project.lib (sprintf "%s_%s" project_name lib_name)
     ~thread
-    ?findlib_deps ?internal_deps ?build_if
+    ?findlib_deps ?internal_deps
     ~dir:(sprintf "lib/%s" lib_name)
-    ~pack_name:(sprintf "%s_%s" name lib_name)
-    ~pkg:(sprintf "%s.%s" name lib_name)
+    ~pack_name:(sprintf "%s_%s" project_name lib_name)
+    ~pkg:(sprintf "%s.%s" project_name lib_name)
 
-let make_app = Item.app ~thread
+let make_app = Project.app ~thread
 
 let async = make_lib "async"
   ~findlib_deps:["async"]
 
 let lwt = make_lib "lwt"
-  ~findlib_deps:["lwt"] ~build_if:[`Pkgs_installed]
+  ~findlib_deps:["lwt"]
 
 let app = make_app "my_app"
   ~file:"app/my_app.ml"
   ~internal_deps:[async]
 
-let project = Project.make ~name ~version [async;lwt;app]
+let optional_pkgs = ["async"; "lwt"]
+
+let items =
+  [async;lwt;app] |>
+  List.filter ~f:(fun x -> Project.dep_opts_sat x optional_pkgs)
 
 ;;
-let () = Project.dispatch project
+let () = Project.basic1 ~project_name ~version items
